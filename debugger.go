8e847ade57bf2fc1c40856da7b9869e5e0ad85e0
root <root@ubuntu.(none)>
2015-04-07 17:23:01 +0800

    Time

--- debugger.go
+++ debugger.go
@@ -2,8 +2,13 @@ package debugger
 
 import "runtime"
 import "fmt"
+import "time"
 
 func Where() string {
 	var _, file, line, _ = runtime.Caller(1)
 	return fmt.Sprintf("%s:%d", file, line)
+}
+
+func Time(begin time.Time) time.Duration {
+	return time.Now().Sub(begin)
 }
